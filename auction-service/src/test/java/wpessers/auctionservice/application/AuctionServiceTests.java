b45d1c9bf72bdcc607d5cb8b055e6d9d2c7ae677
wpessers <warre.pessers@gmail.com>
2025-09-30 21:08:44 +0200

    test(auction): remove assertions of mapping command to domain as it is too verbose

--- auction-service/src/test/java/wpessers/auctionservice/application/AuctionServiceTests.java
+++ auction-service/src/test/java/wpessers/auctionservice/application/AuctionServiceTests.java
@@ -1,6 +1,7 @@
 package wpessers.auctionservice.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 import java.time.Instant;
@@ -30,21 +31,17 @@ class AuctionServiceTests {
     @DisplayName("Should create auction with fields given through command")
     void shouldCreateAuction() {
         // Given
-        CreateAuctionCommand command = new CreateAuctionCommand("Mona Lisa",
+        CreateAuctionCommand command = new CreateAuctionCommand(
+            "Mona Lisa",
             "16th Century painting by Leonardo da Vinci",
-            Instant.parse("2025-01-01T01:00:00Z"));
+            Instant.parse("2025-01-01T01:00:00Z")
+        );
 
         // When
         UUID auctionId = auctionService.createAuction(command);
 
         // Then
-        ArgumentCaptor<Auction> captor = ArgumentCaptor.forClass(Auction.class);
-        verify(auctionStoragePort).save(captor.capture());
-
-        Auction saved = captor.getValue();
-        assertThat(saved.getName()).isEqualTo(command.name());
-        assertThat(saved.getDescription()).isEqualTo(command.description());
-        assertThat(saved.getEndTime()).isEqualTo(command.endTime());
         assertThat(auctionId).isNotNull();
+        verify(auctionStoragePort).save(any(Auction.class));
     }
 }
