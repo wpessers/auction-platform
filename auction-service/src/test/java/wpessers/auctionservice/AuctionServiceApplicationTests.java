5a7bbeb0d00f0d9615a74fd928c2c34bc1a56479
wpessers <warre.pessers@gmail.com>
2025-10-04 09:30:45 +0200

    chore: add testcontainers setup for integration testing

--- auction-service/src/test/java/wpessers/auctionservice/AuctionServiceApplicationTests.java
+++ auction-service/src/test/java/wpessers/auctionservice/AuctionServiceApplicationTests.java
@@ -2,8 +2,12 @@ package wpessers.auctionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestContainersConfiguration.class)
 class AuctionServiceApplicationTests {
 
 	@Test
